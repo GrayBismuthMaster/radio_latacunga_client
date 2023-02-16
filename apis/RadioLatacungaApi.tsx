e7a264a4baaa163422d6d87cccf227886b3704b6
root <graybismuthmaster@hotmail.com>
2023-02-16 17:43:59 -0500

    Logo y nombre

--- apis/RadioLatacungaApi.tsx
+++ apis/RadioLatacungaApi.tsx
@@ -1,5 +1,8 @@
 import axios from 'axios';
 //http://3.12.17.53:5001/api
+// const baseURL= 'http://localhost:5000/api';
+
+const baseURL= 'https://server.radio-latacunga.com/api';
 export default axios.create({
-    baseURL: 'http://localhost:5000/api'
+    baseURL
 })
